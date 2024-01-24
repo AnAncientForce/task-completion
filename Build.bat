4eaa7371542491aa2f125571ab143d583fb2ea33
AnAncientForce <zflyway45@protonmail.com>
2024-01-24 00:04:49 +0000

    Changes

--- Build.bat
+++ Build.bat
@@ -1,3 +1,3 @@
 @echo off
-start explorer "C:\Users\Y\AppData\Local\Temp\electron-packager\win32-x64"
+start explorer "C:\Users\%USERNAME%\AppData\Local\Temp\electron-packager\win32-x64"
 npm run build
